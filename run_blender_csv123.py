62e5aed63787a607e593fca9cb70a977e0605d9c
Hussein HARB <husseinharb124@gmail.com>
2026-06-04 17:06:11 +0200

    Revised

--- run_blender_csv123.py
+++ run_blender_csv123.py
@@ -3,15 +3,13 @@ import os
 import bpy
 
 # Path to the Blender executable
-blender_path = '/home/harb/blender/blender-launcher.sh'  
+blender_path = './blender/blender-launcher.sh'  
 
 # Path to the Blender file you want to run
-#blend_file = '/home/harb/Data/lost+found/Phantom/P145/Electronic files/Phantom_data/MRCP_AM/male_mesh_2025.blend'
-
-blend_file = '/home/harb/blender/male_mesh_2025_dose.blend'
+blend_file = './blender/male_mesh_2025_dose.blend'
 
 # Additional command line arguments
-script_file = '/home/harb/Documents/Python codes/blender_codes/to_do_script_csv123.py'
+script_file = './blender_codes/to_do_script_csv123.py'
 
 # Construct the command
 command = [blender_path, blend_file, '--python', script_file]
